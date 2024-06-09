853e2fb110e526342838d81b8c18bc14df0fd390
Kelvin Kaung <107702120+KelvinKaungDev@users.noreply.github.com>
2024-06-09 23:23:01 +0700

    SignIn, SignUp and SignUpForm UI finished

--- Events.au/Events_auApp.swift
+++ Events.au/Events_auApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Events_auApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SignInView()
         }
     }
 }
---
+++ Events.au/Helpers/EventAppNavigationPathValue.swift
@@ -0,0 +1,14 @@
+//
+//  EventAppNavigationPathValue.swift
+//  Events.au
+//
+//  Created by Kelvin Gao  on 9/6/2567 BE.
+//
+
+import Foundation
+
+struct EventAppNavigationPathValue {
+    static let signUpView = "SignUpView"
+    static let signInView = "SignInView"
+    static let signUpForm = "SignUpForm"
+}
